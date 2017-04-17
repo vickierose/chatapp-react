2873b06aeb6154a9c3d2ea24dd1a0e29d1c23dba
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2017-04-17 11:17:03 +0300

    login + commented sign in

--- src/components/login-page.jsx
+++ src/components/login-page.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
 
 export class LoginPage extends Component {
+    constructor(props){
+        super(props);
+        this.signIn = this.signIn.bind(this);
+    }
+
+    signIn(){
+        // let myHeaders = new Headers(); myHeaders.set('Content-Type', 'application/json');
+
+        // let myInit = {
+        //         method: 'post',
+		// 		headers: myHeaders,
+		// 		mode: 'cors',
+		// 		body: JSON.stringify({"username": 'poiu', "pass": 'zxcv'})
+
+        // };
+
+        // fetch('http://eleksfrontendcamp-mockapitron.rhcloud.com/signup', myInit)
+        // // .then((res) => res.json())
+        // .then(console.log('signed up'));
+
+        let myHeaders = new Headers(); myHeaders.set('Content-Type', 'application/json');
+
+        let myInit = {
+                method: 'post',
+				headers: myHeaders,
+				mode: 'cors',
+				body: JSON.stringify({"username": 'poiu', "password": 'zxcv'})
+
+        };
+
+        fetch('http://eleksfrontendcamp-mockapitron.rhcloud.com/login', myInit)
+        .then((res) => res.json())
+        .then((resObj) => localStorage.setItem('userData', JSON.stringify(resObj)));
+    }
+
     render() {
         return (
             <div className='auth-container'>
                 <h2>Login</h2>
-                <form>
+                <div>
                     <label>
                         <input type="text"
                                 placeholder="Login" 
@@ -17,9 +52,9 @@ export class LoginPage extends Component {
                                 required />
                     </label>
 
-                    <button type="submit">Log In</button>
+                    <button type="submit" onClick={this.signIn}>Log In</button>
                     <button id="google-custom-button">Log in with Google</button>
-                </form>
+                </div>
             </div>
         );
     }

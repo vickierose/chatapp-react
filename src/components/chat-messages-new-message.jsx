b573c78e9c997ea1e214fffb8f79743995bcbb6a
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2017-04-22 13:52:33 +0300

    added handling new message textarea

--- src/components/chat-messages-new-message.jsx
+++ src/components/chat-messages-new-message.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import * as ws from '../utils/ws';
 
 export class CreateNewMessage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            message: ''
+        }
+        this.handleMessageChange = this.handleMessageChange.bind(this);
+    }
+
+    handleMessageChange(e){
+        this.setState(state => ({message: e.target.value}));
+        e.persist();
+    }
+
+    clearForm() {
+    this.setState({ message: '' });
+  }
+
+   submitMessage() {
+    // ws.sendMessage(this.state.message);
+    this.clearForm();
+  }
+
     render() {
         return (
             <form className='create-new-message-form'>
-                <textarea placeholder="Type message" name="text" required></textarea>
+                <textarea value={this.state.message} 
+                          placeholder="Type message"
+                          onChange={this.handleMessageChange} 
+                          required></textarea>
                 <button type="submit">Send</button>
             </form>
         );
---
+++ src/utils/ws.js
@@ -0,0 +1,61 @@
+import io from 'socket.io-client';
+
+export {
+  sendMessage,
+  initConnection,
+  addListener
+};
+
+let socket;
+const listeners = {};
+
+function initConnection(message) {
+  if (!socket) {
+    connect();
+  }
+
+  if (message) {
+    send(message);
+  }
+}
+
+function connect() {
+  socket = io('http://eleksfrontendcamp-mockapitron.rhcloud.com:8000');
+
+  socket.on('message', onMessage);
+  socket.on('join', onJoin);
+  socket.on('leave', onLeave);
+}
+
+function onMessage(msg) {
+  fireListeners('message', msg);
+}
+
+function onJoin(username) {
+  fireListeners('join', username);
+}
+
+function onLeave(username) {
+  fireListeners('leave', username);
+}
+
+function send(message) {
+  socket.emit('message', message);
+}
+
+function sendMessage(message) {
+  initConnection(message);
+}
+
+function fireListeners(event, payload) {
+  if (listeners[event]) {
+    [...listeners[event]].forEach(listener => listener(payload));
+  }
+}
+
+function addListener(event, listener) {
+  if (!listeners[event]) {
+    listeners[event] = [];
+  }
+  listeners[event].push(listener);
+}

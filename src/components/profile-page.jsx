be00fecf6803331ef70ee68476d40f48c3b3a8de
Viktoriia Chernyshuk <vickierose.ch@gmail.com>
2017-05-15 14:36:35 +0300

    added template and basic styles for profile page

--- src/components/profile-page.jsx
+++ src/components/profile-page.jsx
@@ -5,6 +5,11 @@ import { push } from 'react-router-redux';
 
  class ProfilePage extends Component {
 
+     handleSubmit(e){
+        console.log('Saved!');
+        e.preventDefault();
+     }
+
     componentWillMount() {
         if(!this.props.login.user){
             this.props.push('/login')
@@ -13,7 +18,26 @@ import { push } from 'react-router-redux';
 
     render() {
         return (
-            <h1>Profile</h1>
+            <div className='profile-page'>
+                <h2>Profile</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <span>Username</span>
+                        <input type="text" />
+                    </label>
+                    <label>
+                        <span>Status</span>
+                        <input type="text" />
+                    </label>
+                    <label>
+                        <span>Avatar</span>
+                        <input type="file" />
+                    </label>
+                    <button type="submit">Save changes</button>
+                </form>
+            </div>
+
+
         );
     }
 }
